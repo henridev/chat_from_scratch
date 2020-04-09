f3a6a91b6f87138961e506cc5a50b09df6bc7c19
henridev <henri.dbel@gmail.com>
2020-04-09 10:27:15 +0200

    client side chat

--- server/master.js
+++ server/master.js
@@ -1,12 +1,15 @@
 const cluster = require("cluster");
 const os = require("os");
 const WebSocket = require("ws");
+let wssConnections = {};
+let counter = 0;
 
 if (cluster.isMaster) {
   const cpuCount = os.cpus().length;
   for (let i = 0; i < cpuCount; i++) {
     cluster.fork();
   }
+
   //#region startup logs
   Object.values(cluster.workers).forEach((worker, i) =>
     console.log(`worker ${i} : `, worker.process.pid)
@@ -15,17 +18,11 @@ if (cluster.isMaster) {
   console.log("working directory: ", process.cwd());
   console.log("cpus", cpuCount);
   //#endregion
+
   cluster.on("exit", handleWorkerExit);
-
-  //#region set up a single tcp server
-  //   require("./tcpchatserver");
-  /*
-    this will be a server that is created once while the loads for the http will be 
-    distributed accross the x created processes 
-  */
-  //#endregion
-
   const wss = new WebSocket.Server({ port: 1337 });
+  wss.on("listening", () => console.log("WSS listening on 1337"));
+  wss.on("connection", handleWSSConnection);
 } else {
   require("./servernode");
 }
@@ -37,3 +34,51 @@ function handleWorkerExit(worker, code, signal) {
     console.log("new worker created");
   }
 }
+
+function handleWSSConnection(wss) {
+  wss.id = counter++;
+  wssConnections[wss.id] = wss;
+  console.log("wssConnection[wss.id]", wssConnections[wss.id]);
+  wss.on("message", function incoming(message) {
+    const jsonmsg = JSON.parse(message);
+    console.log("message", message);
+    const currentConnection = wssConnections[wss.id];
+    if (!currentConnection.name) {
+      currentConnection.name = jsonmsg.username;
+      console.log("currentConnection", currentConnection);
+      console.log("wss.id", wss.id);
+      sendUserJoined(wss.id);
+    } else {
+      console.log("here");
+      sendAll(jsonmsg.message, wss.id);
+    }
+  });
+}
+
+function sendUserJoined(Id) {
+  for (let i = 0; i < Object.keys(wssConnections).length; i++) {
+    const wssConnection = wssConnections[i];
+    if (Id !== wssConnection.id) {
+      console.log("Id", Id);
+      wssConnection.send(`${wssConnections[Id].name} ---- joined chat`);
+    }
+  }
+}
+
+function sendAll(message, Id) {
+  const username = wssConnections[Id].name;
+  for (let i = 0; i < Object.keys(wssConnections).length; i++) {
+    const wssConnection = wssConnections[i];
+    if (Id !== wssConnection.id) {
+      wssConnection.send(`${username} says ----> ${message}`);
+    }
+  }
+}
+
+//#region set up a single tcp server
+//   require("./tcpchatserver");
+/*
+    this will be a server that is created once while the loads for the http will be 
+    distributed accross the x created processes 
+  */
+//#endregion
